c8024d853c15dd7770bcf08765be5b55b4973ae4
harison1224 <harisonochieng0@gmail.com>
2024-06-30 10:33:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
+console.log(multiply)
+let random = 0;
+let math = random
+console.log(random)
+let num3 = 16
+let num4 = 4
+let mod = num3 / num4
+console.log(mod)
+let max = (1, 5, 10, 15, 20);
+let floor = max 
+console.log(max) 
